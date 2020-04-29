abc1f788dcc208795fc1b8542a26687bc42228ed
Ludvig Åberg Aas <ludaa951@student.liu.se>
2020-04-29 22:16:29 +0200

    Upload New File

---
+++ lab1-spark/labb_1_ex_2_a.py
@@ -0,0 +1,15 @@
+
+from pyspark import SparkContext
+
+
+sc = SparkContext(appName = "exercise test")
+temp_file = sc.textFile("BDA/input/temperature-readings.csv")
+lines = temp_file.map(lambda line: line.split(";"))
+year_temp = lines.map(lambda x: ((x[1][0:4], x[1][5:7]), float(x[3])))
+year_temp = year_temp.filter(lambda x: int(x[0][0]) >= 1950 and int(x[0][0]) <=$
+over_10 = year_temp.filter(lambda x: int(x[1]) >= 10)
+over_10_count = over_10.map(lambda x: (x[0], 1))
+count = over_10_count.reduceByKey(lambda v1, v2: v1 + v2)
+max_tempsorted = count.sortBy(ascending  = False, keyfunc = lambda k:[1])
+max_tempsorted.saveAsTextFile("BDA/output")
+
