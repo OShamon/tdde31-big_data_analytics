7247870fc3fcd7cc298ce37e1eb9cd2f2d391550
Ludvig Åberg Aas <ludaa951@student.liu.se>
2020-04-29 22:19:29 +0200

    Update labb_1_ex_2_b.py

--- lab1-spark/labb_1_ex_2_b.py
+++ lab1-spark/labb_1_ex_2_b.py
@@ -5,7 +5,7 @@ sc = SparkContext(appName = "exercise test")
 temp_file = sc.textFile("BDA/input/temperature-readings.csv")
 lines = temp_file.map(lambda line: line.split(";"))
 year_temp = lines.map(lambda x: ((x[0], x[1][0:4], x[1][5:7]), float(x[3])))
-year_temp = year_temp.filter(lambda x: int(x[0][1]) >= 1950 and int(x[0][1]) <=$
+year_temp = year_temp.filter(lambda x: int(x[0][1]) >= 1950 and int(x[0][1]) <= 2014)
 over_10 = year_temp.filter(lambda x: int(x[1]) >= 10)
 over_10_count = over_10.map(lambda x: (x[0], 1))
 singel_station = over_10_count.reduceByKey(lambda v1, v2: v1)
