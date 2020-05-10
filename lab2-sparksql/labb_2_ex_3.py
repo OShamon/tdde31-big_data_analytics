ff59dbe18a58ab6dbd37b7f50e43067b2937e808
Olivia Shamon <olish585@student.liu.se>
2020-05-10 15:54:54 +0200

    Update labb_2_ex_3.py

--- lab2-sparksql/labb_2_ex_3.py
+++ lab2-sparksql/labb_2_ex_3.py
@@ -1,23 +1,4 @@
-from pyspark import SparkContext
-from pyspark.sql import SQLContext, Row
-from pyspark.sql import functions as F
 
-sc = SparkContext()
-sqlContext = SQLContext(sc)
-
-file = sc.textFile("BDA/input/temperature-readings.csv")
-
-parts = file.map(lambda l: l.split(";"))
-#year, month, station, avgMonthlyTemperature ORDER BY avgMonthlyTemperature DESC
-                                                                                                                        tempReadings = parts.map(lambda p: Row(station=p[0], date=p[1], year=p[1].split("-")[0], month=p[1].split("-")[1], time$
-schemaTempReadings = sqlContext.createDataFrame(tempReadings)
-schemaTempReadings.registerTempTable("tempReadings")
-
-#Filter out irrelevant years
-schemaTempReadingsYear = schemaTempReadings.where(schemaTempReadings['year'] >= 1960)
-schemaTempReadingsYear = schemaTempReadingsYear.where(schemaTempReadingsYear['year'] <= 2014)
-
-schemaTempReadingsYear = schemaTempReadingsYear.groupBy('year', 'month', 'station').avg('value').orderBy(['station']
 from pyspark import SparkContext
 from pyspark.sql import SQLContext, Row
 from pyspark.sql import functions as F
@@ -37,4 +18,4 @@ schemaTempReadings.registerTempTable("tempReadings")
 schemaTempReadingsYear = schemaTempReadings.where(schemaTempReadings['year'] >= 1960)
 schemaTempReadingsYear = schemaTempReadingsYear.where(schemaTempReadingsYear['year'] <= 2014)
 
-schemaTempReadingsYear = schemaTempReadingsYear.groupBy('year', 'month', 'station').avg('value').orderBy(['avg(value)'], ascending = [1]).show(1000)                                                                                    
+schemaTempReadingsYear = schemaTempReadingsYear.groupBy('year', 'month', 'station').avg('value').orderBy(['station'], ascending = [1]).show(1000)                                                                                    
