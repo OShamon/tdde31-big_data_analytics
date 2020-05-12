372a1328e5394b6fa0b5dd7e6fc65f3f17791bb0
Ludvig Åberg Aas <ludaa951@student.liu.se>
2020-05-12 20:46:19 +0200

    Update labb_2_ex_5.py

--- lab2-sparksql/labb_2_ex_5.py
+++ lab2-sparksql/labb_2_ex_5.py
@@ -9,14 +9,14 @@ stations_Ostergotland = sc.textFile("BDA/input/stations-Ostergotland.csv")
 stations_Ostergotland = stations_Ostergotland.map(lambda l: l.split(";"))
 stations_Ostergotland = stations_Ostergotland.map(lambda x: Row(station=x[0]))
 stations_Ostergotland = sqlContext.createDataFrame(stations_Ostergotland)
-stations_Ostergotland = stations_Ostergotland.registerTempTable("stations_Ostergotland").show()
-
+stations_Ostergotland.registerTempTable("stations_Ostergotland")
+stations_Ostergotland.show()
 
 precipitation_readings = sc.textFile("BDA/input/precipitation-readings.csv")
 precipitation_readings = precipitation_readings.map(lambda l: l.split(";"))
 precipitation_readings = precipitation_readings.map(lambda x: Row(station=x[0], year=x[1][0:4], month=x[1][5:7], value=float(x[3])))
 precipitation_readings = sqlContext.createDataFrame(precipitation_readings)
-precipitation_readings = precipitation_readings.registerTempTable("precipitation_readings")
+precipitation_readings.registerTempTable("precipitation_readings")
 
 
 precipitation_readings = precipitation_readings.where(precipitation_readings['year'] >= 1993)
