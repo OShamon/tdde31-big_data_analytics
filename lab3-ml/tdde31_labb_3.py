813a4a6a667142a7cd16bfba5184fdc7384f45cd
Ludvig Åberg Aas <ludaa951@student.liu.se>
2020-05-19 19:24:57 +0200

    Update tdde31_labb_3.py

--- lab3-ml/tdde31_labb_3.py
+++ lab3-ml/tdde31_labb_3.py
@@ -25,11 +25,8 @@ date = "2013-07-04" # Up to you
 stations = sc.textFile("data/stations.csv") 
 temps = sc.textFile("data/temps.csv")
 # Your code here
-def gaussian_kernel_dist(station_id, station_list, lon, lat, h_distance):
-    for station in station_list:
-        if int(station[0]) == int(station_id):
-            return exp(-((haversine(float(station[1][0]), float(station[1][1]), lon, lat)/h_distance)**2))
-    return None
+def gaussian_kernel_dist(lon_lat, lon, lat, h_distance):
+    return exp(-((haversine(lon_lat[0], lon_lat[1], lon, lat)/h_distance)**2))
 
 def gaussian_kernel_time(time_1, time_2, h_time):
     h1 = int(time_1.split(":")[0])
@@ -40,30 +37,32 @@ def gaussian_kernel_time(time_1, time_2, h_time):
 def gaussian_kernel_date(date_1, date_2, h_date):
     d1 = int(date_1.split("-")[1])*31 + int(date_1.split("-")[2])
     d2 = int(date_2.split("-")[1])*31 + int(date_2.split("-")[2])
-    
-    return exp(((d1 - d2)/h_date)**2)
 
-temperature_file = sc.textFile("BDA/input/temperature-readings-small.csv")
+    return exp(-((d1 - d2)/h_date)**2)
+
+def get_lon_lat(station_list, station_id):
+    for station in station_list:
+        if int(station[0]) == int(station_id):
+            return float(station[1][0]), float(station[1][1])
+
+
+temperature_file = sc.textFile("BDA/input/temperature-readings.csv")
 stations_file = sc.textFile("BDA/input/stations.csv")
-lines_temp = temperature_file.map(lambda line: line.split(";"))
+lines_temp = temperature_file.map(lambda line: line.split(";")).sample(False, 0.1)
 lines_station = stations_file.map(lambda line: line.split(";"))
-
-#Station number, Date, Time, temperature,  Quality
-temprature_readings_test = lines_temp.map(lambda x: (x[0], x[1], x[2], float(x[3])))
-temperature_readings_test = temperature_readings(lambda x: datetime(int(x[1][0:4]), int(x[1][5:7]), int(x[1][8:10])) <= datetime(int(date[0:4]), int(date[5:7]), int(date[8:10])))
 
 stations = lines_station.map(lambda x: (x[0], (x[3], x[4])))
 stations_b = sc.broadcast(stations.collect())
+temperature_readings = lines_temp.map(lambda x: (x[0], x[1], x[2], get_lon_lat(stations_b.value, x[0]), float(x[3])))
+temperature_readings_lon_lat = temperature_readings.filter(lambda x: datetime(int(x[1][0:4]), int(x[1][5:7]), int(x[1][8:10])) <= datetime(int(date[0:4]), int(date[5:7]), int(date$
+temperature_readings_lon_lat.cache()
 
-list_of_predictions = []                                     
-                                     
-for time in ["24:00:00", "22:00:00", "20:00:00", "18:00:00", "16:00:00", "14:00:00",
-"12:00:00", "10:00:00", "08:00:00", "06:00:00", "04:00:00"]:
-    temprature_readings = lines_temp.map(lambda x: ((x[0], x[1][0:4], x[1][5:7], x[1][8:10]), gaussian_kernel_dist(x[0], stations_b.value, a, b, h_distance), gaussian_kernel_date(x[1], date, h_date), gaussian_kernel_time(x[2], time, h_time), float(x[3])))
+list_of_predictions = []
+for time in ["24:00:00", "22:00:00"]: # , "20:00:00", "18:00:00", "16:00:00", "14:00:00", "12:00:00", "10:00:00", "08:00:00", "06:00:00", "04:00:00"]:
+    temprature_readings = temperature_readings_lon_lat.map(lambda x: (x[0], gaussian_kernel_dist(x[3], a, b, h_distance), gaussian_kernel_date(x[1], date, h_date), gaussian_kernel$
     temp_readings = temprature_readings.map(lambda x: (1 , (x[1]*x[4] + x[2]*x[4] + x[3]*x[4], x[1] + x[2] + x[3])))
     #temp_readings.saveAsTextFile("BDA/output")
     reduced =  temp_readings.reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
-    serializzed = reduced
     #serializzed.saveAsTextFile("BDA/output")
     list_of_predictions.append(reduced.collect())
 
@@ -75,3 +74,6 @@ for i in list_of_predictions:
 list_of_sc = sc.parallelize(list_of)
 
 list_of_sc.saveAsTextFile("BDA/output")
+
+
+
